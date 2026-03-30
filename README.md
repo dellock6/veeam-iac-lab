a9d35ded38089ae11f0a3d7984a21f10f5c005e2
Luca Dell'Oca <ldelloca@gmail.com>
2026-03-30 15:34:18 +0200

    New terraform plan to deploy pyKMIP over a Ubuntu 24.04 machine

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # veeam-iac-lab
 
-Here I'm collecting all the components for creating a small Veeam lab:
+Here I'm collecting all the automation scripts that I create for my home lab.
+What' available?
 
-packer:
+Packer:
   - ubuntu2204: create a Ubuntu 22.04 template using Packer
   - win2019: create a Windows 2019 template using Packer
 
-terraform:
+Terraform:
   - vbr: deploy a Windows VM for VBR (Veeam Backup & Replication) Server using Terraform
-  - lhr: deploy a Ubuntu Linux 22.04 VM with additional disk formatted with XFS and reflink enabled for Linux Hardened Repository
+  - lhr: deploy a Ubuntu Linux 22.04 VM with one additional disk formatted with XFS and reflink enabled for Linux Hardened Repository
   - aws-s3: create a aws S3 bucket with object lock
+  - pykmip: build a Ubuntu Linux 24.04 VM and installs pyKMIP into it, to build a complete KMS server for personal use
---
+++ terraform/pykmip/main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_version = ">= 0.14.7"
+  required_providers {
+    vsphere = {
+      source = "vmware/vsphere"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  count         = var.vsphere_cluster == "" ? 0 : 1
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_host" "esxihost" {
+  count         = var.vsphere_cluster == "" ? 1 : 0
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "pykmip" {
+  name             = var.host_name
+  resource_pool_id = var.vsphere_cluster == "" ? data.vsphere_host.esxihost[0].resource_pool_id : data.vsphere_compute_cluster.cluster[0].resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = var.host_vm_folder
+
+  num_cpus  = var.host_cpu
+  memory    = var.host_ram_mb
+  guest_id  = data.vsphere_virtual_machine.template.guest_id
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  firmware  = data.vsphere_virtual_machine.template.firmware
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    unit_number      = 0
+    size             = var.host_disk_gb
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+
+  cdrom {
+    client_device = true
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = var.host_name
+        domain    = var.host_domain
+      }
+
+      network_interface {
+        ipv4_address = var.host_ip
+        ipv4_netmask = var.host_subnet
+      }
+
+      ipv4_gateway    = var.host_gateway
+      dns_server_list = var.dns_server_list
+      dns_suffix_list = var.dns_suffix_list
+    }
+  }
+
+  connection {
+    type     = "ssh"
+    agent    = "false"
+    host     = var.host_ip
+    user     = var.host_user
+    password = var.host_password
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/provision_pykmip.sh"
+    destination = "/tmp/provision_pykmip.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo bash /tmp/provision_pykmip.sh ${var.host_ip} ${var.host_name} ${var.host_domain}"
+    ]
+  }
+}
